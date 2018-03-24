fd78ea69a558afdb85a600f6d9e3ddcda15e040a
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2018-03-24 00:07:27 -0300

    feat(show errors): the errors messages now is sent to stderr

--- lib/error/show_errors.ex
+++ lib/error/show_errors.ex
@@ -3,12 +3,14 @@ defmodule MacroCompiler.Error.ShowErrors do
 
   alias MacroCompiler.Parser.SyntaxError
 
+  defp puts_stderr(message), do: IO.puts(:stderr, message)
+
   ###
   # Compiler-time error message
   def show(file, %SyntaxError{message: message, line: line, offset: offset}) do
     {line, col} = calc_line_and_column(file, line, offset)
 
-    IO.puts IO.ANSI.format([:red, :bright, "#{message}\n"])
+    puts_stderr IO.ANSI.format([:red, :bright, "#{message}\n"])
 
     file
     |> String.split("\n")
@@ -16,18 +18,19 @@ defmodule MacroCompiler.Error.ShowErrors do
     |> Enum.filter(fn {_, index} ->
       index >= line - 2 and index <= line + 2
     end)
-    |> Enum.each(fn {lineText, index} ->
+    |> Enum.map(fn {lineText, index} ->
       if index == line do
         lineTextSliced0 = String.slice(lineText, 0..(col-1))
         lineTextSliced1 = String.slice(lineText, col..String.length(lineText))
 
-        IO.puts IO.ANSI.format([:bright, "#{index}  -  ", lineTextSliced0, :red,  lineTextSliced1], true)
+        IO.ANSI.format([:bright, "#{index}  -  ", lineTextSliced0, :red,  lineTextSliced1], true)
       else
-        IO.puts "#{index}  -  #{lineText}"
+        "#{index}  -  #{lineText}"
       end
     end)
+    |> Enum.each(&puts_stderr/1)
 
-    IO.puts "\n\nMacro couldn't be compiled. Sorry"
+    puts_stderr "\n\nMacro couldn't be compiled. Sorry"
   end
 
   ###
@@ -40,7 +43,7 @@ defmodule MacroCompiler.Error.ShowErrors do
       %{type: :warning, message: message, metadatas: metadatas} ->
         [warning_message_prefix | message] ++ " #{metadates_to_line_column_message(file, metadatas)}"
         |> IO.ANSI.format
-        |> IO.puts
+        |> puts_stderr
     end)
   end
 
