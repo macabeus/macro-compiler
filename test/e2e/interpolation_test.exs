a470037860d8b6281c7b3ccac115239878575fe0
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2018-07-08 17:21:47 -0300

    test-e2e: add interpolation test

---
+++ test/e2e/interpolation_test.exs
@@ -0,0 +1,29 @@
+defmodule MacroCompiler.Test.E2e.Interpolation do
+  use MacroCompiler.Test.E2e.Helper
+
+  def code, do: """
+    macro Test {
+      $scalar = foo
+      log scalar: $scalar
+
+      @array = (1, 2)
+      log array: @array
+
+      %hash = (1 => foo, 2 => bar)
+      log hash: %hash
+    }
+  """
+
+  test_output :string, "should can interpolate a scalar", fn value ->
+    value == "scalar: foo"
+  end
+
+  test_output :string, "should can interpolate an array", fn value ->
+    value == "array: 2"
+  end
+
+  test_output :string, "should can interpolate a hash", fn value ->
+    value == "hash: 2"
+  end
+end
+
