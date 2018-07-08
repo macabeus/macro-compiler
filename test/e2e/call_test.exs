d8f8dcdcebe572fb8a3a7a97beb4893adda5e50c
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2018-07-08 17:21:30 -0300

    test-e2e: add call test

---
+++ test/e2e/call_test.exs
@@ -0,0 +1,28 @@
+defmodule MacroCompiler.Test.E2e.Call do
+  use MacroCompiler.Test.E2e.Helper
+
+  def code, do: """
+    macro Test {
+      log start
+      call Called
+      log end
+    }
+
+    macro Called {
+      log called
+    }
+  """
+
+  test_output :string, "should startly print 'start'", fn value ->
+    value == "start"
+  end
+
+  test_output :string, "should print 'called'", fn value ->
+    value == "called"
+  end
+
+  test_output :string, "should print at end 'end'", fn value ->
+    value == "end"
+  end
+end
+
