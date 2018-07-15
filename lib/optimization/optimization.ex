f6f02dcf9757da6492600c81be3cab2917869b28
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2018-07-15 13:22:55 -0300

    optimization: rename "build_ast_otimatized" to "build_ast_optimized"

--- lib/optimization/optimization.ex
+++ lib/optimization/optimization.ex
@@ -3,7 +3,7 @@ defmodule MacroCompiler.Optimization do
   alias MacroCompiler.Optimization.ConstantFolding
   alias MacroCompiler.SemanticAnalysis
 
-  def build_ast_otimatized(ast) do
+  def build_ast_optimized(ast) do
     symbols_table = SemanticAnalysis.build_symbols_table(ast)
     new_ast =
       ast
@@ -13,7 +13,7 @@ defmodule MacroCompiler.Optimization do
     if new_ast == ast do
       new_ast
     else
-      build_ast_otimatized(new_ast)
+      build_ast_optimized(new_ast)
     end
   end
 end
