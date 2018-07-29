35b61cdc1c3a1f2a866fc97fb70eab89b42d738a
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2018-07-29 17:02:55 -0300

    optimization: refactor to better readability

--- lib/optimization/optimization.ex
+++ lib/optimization/optimization.ex
@@ -3,28 +3,38 @@ defmodule MacroCompiler.Optimization do
   alias MacroCompiler.Optimization.ConstantFolding
   alias MacroCompiler.SemanticAnalysis
 
-  def build_ast_optimized(ast, optimization) do
-    symbols_table = SemanticAnalysis.build_symbols_table(ast)
-    new_ast = optimization.optimize(ast, symbols_table)
+  # Run all optimizations
+  def build_ast_optimized(ast) do
+    build_ast_optimized(ast, [DeadCodeStrip, ConstantFolding])
+  end
 
-    if new_ast == ast do
-      new_ast
+  # Run a couple of optimizations
+  def build_ast_optimized(ast, opts) when is_list(opts) do
+    symbols_table = SemanticAnalysis.build_symbols_table(ast)
+
+    optimized_ast =
+      Enum.reduce(opts, ast, fn (opt, current_ast) ->
+        opt.optimize(current_ast, symbols_table)
+      end)
+
+    if optimized_ast == ast do
+      optimized_ast
     else
-      build_ast_optimized(new_ast, optimization)
+      build_ast_optimized(optimized_ast, opts)
     end
   end
 
-  def build_ast_optimized(ast) do
+  # Run a single optimization
+  def build_ast_optimized(ast, opt) when is_atom(opt) do
     symbols_table = SemanticAnalysis.build_symbols_table(ast)
-    new_ast =
-      ast
-      |> DeadCodeStrip.optimize(symbols_table)
-      |> ConstantFolding.optimize(symbols_table)
 
-    if new_ast == ast do
-      new_ast
+    optimized_ast =
+      opt.optimize(ast, symbols_table)
+
+    if optimized_ast == ast do
+      optimized_ast
     else
-      build_ast_optimized(new_ast)
+      build_ast_optimized(optimized_ast, opt)
     end
   end
 end
